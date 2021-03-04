2b30cd09e46ba36d4e82037473300bdb87480722
Marcio Nitao <marcionitao@gmail.com>
2021-03-05 00:29:45 +0100

    code reorganization

--- src/components/AppItemList.vue
+++ src/components/AppItemList.vue
@@ -1,6 +1,39 @@
 <template>
   <div>
-    AppItemList: {{ items }}
+    
+    <h5>{{ title }} <span class="badge badge-info">{{ items.length }}</span></h5>
+    <div class="card">
+      <div class="card-body">
+        <!--List Item-->
+        <ul class="list-group">
+          <li class="list-group-item" v-for="item in items" v-bind:key="item">
+              <!--Define Item title and delete icon-->
+            <div class="row">
+              <div class="col-md">
+                {{ item }}
+              </div>
+              <div class="col-md text-right">
+                <button class="btn btn-info" v-on:click="deleteItem(item)">
+                  <span class="fa fa-trash"></span>
+                </button>
+              </div>
+            </div>
+          </li>
+        </ul>
+        <br>
+        <!--input form to Item-->
+        <div class="input-group">
+          <input class="form-control" v-model="item" v-on:keyup.enter="addItem(item)" type="text" placeholder="Add Item">
+          <div class="input-group-append">
+            <button class="btn btn-danger" @click="addItem(item)">
+              <span class="fa fa-plus"></span>
+            </button>
+          </div>
+        </div>
+
+      </div>
+    </div>
+
   </div>
 </template>
 
@@ -10,7 +43,24 @@ export default{
 
   name: 'AppItemList',
   //Props allow you to pass a value (state) from one component (parent) to another (child).
-  props: ['items'],
+  props: ['title','items'],
+  data() {
+    return {
+      item: '',
+    };
+  },
+  methods: {
+    // 'addItem' mirror 'addPrefix' and 'addSufix'
+    addItem(item: string) {
+      // trigged a event to a next action
+      this.$emit('addItem', item);
+      // to clean
+      this.item = '';
+    },
+    deleteItem(item:any): void {
+      this.$emit('deleteItem', item);
+    }
+  }
 };
 </script>
 
---
+++ src/components/DomainList.vue
@@ -0,0 +1,111 @@
+<template>
+  <div>
+
+    <div id="main">
+      <div class="container">
+        <!-- 1 row -->
+        <div class="row">
+          <!-- 1ª columm -->
+          <div class="col-md">
+            <!--add component of Items and send datas for him then call a event to a next action-->
+            <AppItemList title='prefixes' v-bind:items="prefixes" v-on:addItem="addPrefix" v-on:deleteItem="deletePrefix"></AppItemList>
+          </div>
+           <!-- 2ª columm -->
+          <div class="col-md">
+            <!--add component of Items and send datas for him then call a event to a next action-->
+            <AppItemList title='sufixes' v-bind:items="sufixes" v-on:addItem="addSufix" v-on:deleteItem="deleteSufix"></AppItemList>
+          </div>
+
+        </div>
+        <br>
+
+        <h5>Domains <span class="badge badge-info">{{ domains.length }}</span></h5>
+        <div class="card">
+          <div class="card-body">
+            <ul class="list-group">
+              <li class="list-group-item" v-for="domain in domains" v-bind:key="domain.name">
+                <!--add button to save domain-->
+                <div class="row">
+                  <div class="col-md">
+                    {{ domain.name }}
+                  </div>
+                </div>
+                <div class="col-md text-right">
+                  <a class="btn btn-warning" v-bind:href="domain.checkout" target="_blank">
+                    <span class="fa fa-shopping-cart"></span>
+                  </a>
+                </div>
+
+              </li>
+            </ul>
+          </div>
+        </div>
+
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'font-awesome/css/font-awesome.css';
+import AppItemList from './AppItemList.vue';
+
+export default Vue.extend({
+  name: 'app',
+  components: {
+    AppItemList
+  },
+  data() {
+    return {
+      prefixes: ['Air', 'Jet', 'Flight'],
+      sufixes: ['Hub', 'Station', 'Mart'],     
+    };
+  },
+  methods: {
+
+    addPrefix(prefix: any) {
+      this.prefixes.push(prefix);
+    },
+    deletePrefix(prefix: any) {
+      this.prefixes.splice(this.prefixes.indexOf(prefix),1);
+    },
+    addSufix(sufix: any) {
+      this.sufixes.push(sufix);
+    },
+    deleteSufix(sufix: any) {
+      this.sufixes.splice(this.sufixes.indexOf(sufix),1);
+    },
+    
+  },
+  // is ideal to use dynamic interaction and reactive datas, Example: as data input on the screen or updates
+  computed: {    
+    domains(): any {
+      const domains = [];
+      for (const prefix of this.prefixes) {
+        for (const sufix of this.sufixes) {
+          const name = (prefix + sufix);
+          // all letters of the same size
+          const url = name.toLowerCase();
+          const checkout = `https://checkout.hostgator.com.br/?a=add&sld=${url}&tld=.com.br`;
+          //create a Object
+          domains.push({
+            name,
+            checkout
+          });
+        }      
+      }
+      return domains;
+    }
+  },
+  // 'created' is run after of 'data()', 'methods()'... is ideal to get datas from backend or using APIs
+  // created() {
+
+// },
+});
+</script>
+
+<style scoped>
+
+</style>
